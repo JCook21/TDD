e9c895e4afaad56baa81117cee261de1b94d01d7
Cook, Jeremy <jeremy.cook@oracle.com>
2022-02-24 13:13:39 -0500

    Implemented algorithm to convert roman numerals to Arabic numbers.

--- src/main/java/RomanNumeralConverter.java
+++ src/main/java/RomanNumeralConverter.java
@@ -1,5 +1,18 @@
+import java.util.Map;
+
 public class RomanNumeralConverter {
-    public int convertRomanNumeralToArabicNumber(String i) {
-        return 0;
+    private final Map<String, Integer> romanDigitToNumber = Map.of("I", 1, "V", 5, "X", 10, "L", 50, "C", 100);
+
+    public int convertRomanNumeralToArabicNumber(String romanNumeral) {
+        var first = romanDigitToNumber.get(romanNumeral.substring(0, 1));
+        if (romanNumeral.length() == 1) {
+            return first;
+        }
+        var second = romanDigitToNumber.get(romanNumeral.substring(1, 2));
+        if (second > first) {
+            first = Math.negateExact(first);
+        }
+
+        return first + convertRomanNumeralToArabicNumber(romanNumeral.substring(1));
     }
 }
--- src/test/java/RomanNumeralConverterTest.java
+++ src/test/java/RomanNumeralConverterTest.java
@@ -1,12 +1,34 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RomanNumeralConverterTest {
-    @Test
-    void convertRomanNumeralToArabic() {
+    static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                Arguments.of("I", 1),
+                Arguments.of("II", 2),
+                Arguments.of("III", 3),
+                Arguments.of("IV", 4),
+                Arguments.of("V", 5),
+                Arguments.of("VI", 6),
+                Arguments.of("IX", 9),
+                Arguments.of("X", 10),
+                Arguments.of("XIX", 19),
+                Arguments.of("L", 50),
+                Arguments.of("C", 100),
+                Arguments.of("CLVI", 156)
+        );
+    }
+
+    @ParameterizedTest(name = "It converts {0} to {1}")
+    @MethodSource("dataProvider")
+    void convertRomanNumeralToArabic(String romanNumeral, int expectedArabicNumber) {
         var converter = new RomanNumeralConverter();
 
-        assertEquals(1, converter.convertRomanNumeralToArabicNumber("I"));
+        assertEquals(expectedArabicNumber, converter.convertRomanNumeralToArabicNumber(romanNumeral));
     }
 }
